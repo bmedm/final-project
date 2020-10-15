e7ce3a16217f63514fc5dff10b72f164f867b44a
bmedm <b.medialm@hotmail.es>
2020-10-15 12:36:51 +0200

    precios combustible

--- src/controllers/pCO2bcontroller.py
+++ src/controllers/pCO2bcontroller.py
@@ -6,14 +6,15 @@ from src.database import db
 import src.outputhtml as so
 from bson.json_util import dumps
 import json
-import webbrowser
 import src.calls.folium as f
 import pandas as pd 
 from dotenv import load_dotenv
 load_dotenv()
 from folium import Map, Marker, Icon, FeatureGroup, LayerControl, Choropleth,PolyLine,Element
 from folium.plugins import HeatMap
-import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
 
 
  
@@ -26,7 +27,15 @@ def welcome():
 @app.route("/prediction/direction",methods=["GET","POST"])
 def mapa_walking():
     cars=pd.read_csv("./CO2Emissions_vehi.csv")
-    
+    url = 'https://gasolinabarata.info/precio-gasolina/'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    tag=soup.select('.bloqueGasolina div')
+    regular_gasoline= float(tag[2].text.strip().strip("€/l"))
+    premium_gasoline=float(tag[5].text.strip().strip("€/l"))
+    diesel=float(tag[8].text.strip().strip("€/l"))
+    ethanol=float(tag[20].text.strip().strip("€/l"))
+    gas_natural=float(tag[23].text.strip().strip("€/l"))
 
     fueltypes={"Premium gasoline":"Z", 
             "Regular gasoline":"X",
@@ -55,55 +64,62 @@ def mapa_walking():
         fueltype=request.args.get("Fuel")
         mood=request.args.get('mood')
     
-    
+    prices={"X":regular_gasoline,
+            "Z":premium_gasoline,
+            "D":diesel,
+            "E":ethanol,
+            "N":gas_natural}
 
     if modo=="walking":
         res=f.walk(origin=origin,destination=destination,alternative=modo) 
         distancia=f.hi_google(origin=origin,destination=destination,alternative="driving")
         distancia=distancia[0]
         co2=cars[(cars["Make"]==marca) & (cars["Model"]==modelo) & (cars["Fuel Type"]==fuel)]
+        save=round((co2["Fuel Consumption City (L/100 km)"].mean()/100)*prices[fuel]*distancia,2)
         corrco2=co2["CO2 Emissions(g/km)"].sum()
         if corrco2==0:
             co2=cars[(cars["Make"]==marca)&(cars["Fuel Type"]==fuel)]
             co2=round((co2["CO2 Emissions(g/km)"].mean())*distancia,3)
-            return so.html_normal(res=res,co2=co2,mood=mood)
+            return so.html_walk(res=res,co2=co2,mood=mood,fuel=save)
     
         else:
             co2=co2
             co2=round((co2["CO2 Emissions(g/km)"].mean())*distancia,3)
-            return so.html_normal(res=res,co2=co2,mood=mood)
+            return so.html_walk(res=res,co2=co2,mood=mood,fuel=save)
 
     if modo=="driving":
         res=f.walk(origin=origin,destination=destination,alternative=modo) 
         distancia=f.hi_google(origin=origin,destination=destination,alternative="driving")
         distancia=distancia[0]
-        co2c=cars[(cars["Make"]==marca) & (cars["Model"]==modelo) & (cars["Fuel Type"]==fuel)]
-        corrco2c=co2c["CO2 Emissions(g/km)"].sum()
+        co2=cars[(cars["Make"]==marca) & (cars["Model"]==modelo) & (cars["Fuel Type"]==fuel)]
+        save=round((co2["Fuel Consumption City (L/100 km)"].mean()/100)*prices[fuel]*distancia,2)
+        corrco2c=co2["CO2 Emissions(g/km)"].sum()
         if corrco2c==0:
-            co2c=cars[(cars["Make"]==marca)&(cars["Fuel Type"]==fuel)]
-            co2c=round((co2c["CO2 Emissions(g/km)"].mean())*distancia,3)
-            return so.html_driving(res=res,co2=co2,mood=mood)
+            co2=cars[(cars["Make"]==marca)&(cars["Fuel Type"]==fuel)]
+            co2=round((co2["CO2 Emissions(g/km)"].mean())*distancia,3)
+            return so.html_driving(res=res,co2=co2,mood=mood,fuel=save)
     
         else:
-            co2c=co2c
-            co2c=round((co2c["CO2 Emissions(g/km)"].mean())*distancia,3)
-            return so.html_driving(res=res,co2=co2,mood=mood)
+            co2=co2
+            co2=round((co2["CO2 Emissions(g/km)"].mean())*distancia,3)
+            return so.html_driving(res=res,co2=co2,mood=mood,fuel=save)
     if modo=="bicycling":
         res=f.bicycling(origin=origin,destination=destination,alternative=modo) 
         distancia=f.hi_google(origin=origin,destination=destination,alternative="driving")
         distancia=distancia[0]
         co2=cars[(cars["Make"]==marca) & (cars["Model"]==modelo) & (cars["Fuel Type"]==fuel)]
+        save=round((co2["Fuel Consumption City (L/100 km)"].mean()/100)*prices[fuel]*distancia,2)
         corrco2=co2["CO2 Emissions(g/km)"].sum()
 
         if corrco2==0:
             co2=cars[(cars["Make"]==marca)&(cars["Fuel Type"]==fuel)]
             co2=round((co2c["CO2 Emissions(g/km)"].mean())*distancia,3)
-            return so.html_normal(res=res,co2=co2,mood=mood)
+            return so.html_bike(res=res,co2=co2,mood=mood,fuel=save)
     
         else:
             co2=co2
             co2=round((co2["CO2 Emissions(g/km)"].mean())*distancia,3)
-            return so.html_normal(res=res,co2=co2,mood=mood)
+            return so.html_bike(res=res,co2=co2,mood=mood,fuel=save)
         
         
 
--- src/outputhtml.py
+++ src/outputhtml.py
@@ -1,15 +1,15 @@
 
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 
-def html_normal(res,co2,mood):
+def html_walk(res,co2,mood,fuel):
     aviones=pd.read_csv("./aviones.csv")
     travel=aviones[aviones["Estilo"]==mood]
     travel=travel.sample(3)
     avco2=[i for i in travel["CO2/pass"]]
     avurl=[i for i in travel["url"]]
     avdest=[i for i in travel["Destino"]]
-  
-
     return f"""<!DOCTYPE html>
             <html lang="es">
             <head>
@@ -35,8 +35,8 @@ def html_normal(res,co2,mood):
             <div style="text-align:center;">
                 <table style="margin: 0 auto;" cellspacing="100">
                     <tr>
-                        <td>
-                            <h1>You save:{co2} g of CO2<h1>
+                        <td WIDTH="20" HEIGHT="20">
+                            <h1>You save:{co2} g of CO2 and {round(fuel,2)}€<h1>
                         </td>
                         <td>
                             <h1>{res._repr_html_()}<h1>
@@ -59,24 +59,24 @@ def html_normal(res,co2,mood):
                 </tr>
                 <tr>
                     <td>
-                        <img width="300" height="300" src="{avurl[0]}" alt="travel">
+                        <img width="400" height="300" src="{avurl[0]}" alt="travel">
                     </td>
                     <td>
-                        <img width="300" height="300" src="{avurl[1]}" alt="travel">
+                        <img width="400" height="300" src="{avurl[1]}" alt="travel">
                     </td>
                     <td>
-                        <img width="300" height="300" src="{avurl[2]}" alt="travel">
+                        <img width="400" height="300" src="{avurl[2]}" alt="travel">
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivale a {int((avco2[0]/co2)+1)} veces este mismo trayecto en gasto de CO2 en avión<h2>
+                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivalent to {int((avco2[0]/co2)+1)} times this same journey in CO2 expenditure by plane and you would save  {round(int((avco2[0]/co2)+1)*fuel,2)}€<h2>
                     </td>
                     <td>
-                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivale a {int((avco2[1]/co2)+1)} veces este mismo trayecto en gasto de CO2 en avión<h2>
+                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivalent to {int((avco2[1]/co2)+1)} times this same journey in CO2 expenditure by plane and you would save  {round(int((avco2[0]/co2)+1)*fuel,2)}€<h2>
                     </td>
                     <td>
-                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivale a {int((avco2[2]/co2)+1)} veces este mismo trayecto en gasto de CO2 en avión<h2>
+                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivalent to {int((avco2[2]/co2)+1)} times this same journey in CO2 expenditure by plane and you would save {round(int((avco2[0]/co2)+1)*fuel,2)}€<h2>
                     </td>
                 </tr>
             </table>
@@ -84,7 +84,7 @@ def html_normal(res,co2,mood):
             </body>
             </html>"""
 
-def html_driving(res,co2,mood):
+def html_driving(res,co2,mood,fuel):
     aviones=pd.read_csv("./aviones.csv")
     travel=aviones[aviones["Estilo"]==mood]
     travel=travel.sample(3)
@@ -118,8 +118,8 @@ def html_driving(res,co2,mood):
             <div style="text-align:center;">
                 <table style="margin: 0 auto;" cellspacing="100">
                     <tr>
-                        <td>
-                            <h1>ARE YOU SURE???: you are emitting {co2}g of CO2 to the planet</h1>
+                        <td WIDTH="20" HEIGHT="20">
+                            <h1>ARE YOU SURE???:puf! {co2}g of CO2 to the planet!</h1>
                         </td>
                         <td>
                             <h1>{res._repr_html_()}<h1>
@@ -142,24 +142,104 @@ def html_driving(res,co2,mood):
                 </tr>
                 <tr>
                     <td>
-                        <img width="300" height="300" src="{avurl[0]}" alt="travel">
+                        <img width="400" height="300" src="{avurl[0]}" alt="travel">
                     </td>
                     <td>
-                        <img width="300" height="300" src="{avurl[1]}" alt="travel">
+                        <img width="400" height="300" src="{avurl[1]}" alt="travel">
                     </td>
                     <td>
-                        <img width="300" height="300" src="{avurl[2]}" alt="travel">
+                        <img width="400" height="300" src="{avurl[2]}" alt="travel">
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivale a {int((avco2[0]/co2)+1)} veces este mismo trayecto en gasto de CO2 en avión<h2>
+                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivalent to {int((avco2[0]/co2)+1)} times this same journey in CO2 expenditure by plane and you would save  {round(int((avco2[0]/co2)+1)*fuel,2)}€<h2>
                     </td>
                     <td>
-                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivale a {int((avco2[1]/co2)+1)} veces este mismo trayecto en gasto de CO2 en avión<h2>
+                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivalent to {int((avco2[1]/co2)+1)} times this same journey in CO2 expenditure by plane and you would save  {round(int((avco2[1]/co2)+1)*fuel,2)}€<h2>
                     </td>
                     <td>
-                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivale a {int((avco2[2]/co2)+1)} veces este mismo trayecto en gasto de CO2 en avión<h2>
+                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivalent to {int((avco2[2]/co2)+1)} times this same journey in CO2 expenditure by plane and you would save  {round(int((avco2[2]/co2)+1)*fuel,2)}€<h2>
+                    </td>
+                </tr>
+            </table>
+            </div>
+            </body>
+            </html>"""
+def html_bike(res,co2,mood,fuel):
+    aviones=pd.read_csv("./aviones.csv")
+    travel=aviones[aviones["Estilo"]==mood]
+    travel=travel.sample(3)
+    avco2=[i for i in travel["CO2/pass"]]
+    avurl=[i for i in travel["url"]]
+    avdest=[i for i in travel["Destino"]]
+    return f"""<!DOCTYPE html>
+            <html lang="es">
+            <head>
+            <meta charset="utf-8">
+            <title>PiggyCO2bank</title>
+            <link rel="icon"type="image/png" href="https://cdn4.iconfinder.com/data/icons/business-economy-market-company-filling-wiht-beaut/283/69-512.png">
+            </head>
+           <body style="margin:50px;text-align:center; background:#cff6fd; 
+            background-image:url('https://i.pinimg.com/originals/39/dd/63/39dd637ade2a7f5d4a3b74c0664be29a.jpg');
+            background-repeat:no-repeat ;
+            background-size: 100% 100%;
+            color:#000000;
+            font-family:monospace,arial,helvética; ">
+                    <h1 style="color:#eeb8ec;font-family:copperplate; font-size: 50px; "><code>Piggy<big>|CO2|</big>bank</code></h1>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" style="padding:4px; background-color:whitesmoke; text-align:center;">
+                    <p><b>How much are you going to save the planet?</b></p>
+                </td>
+            </tr>
+            </table>
+            <div style="text-align:center;">
+                <table style="margin: 0 auto;" cellspacing="100">
+                    <tr>
+                        <td WIDTH="20" HEIGHT="20">>
+                            <h1 >You save:{co2} g of CO2 and {round(fuel-0.5,2)}€<h1>
+                        </td>
+                        <td>
+                            <h1>{res._repr_html_()}<h1>
+                        </td>
+                    </tr>
+            </table>
+    
+            <h1 style="color:#e2748c;font-family:arial; font-size: 50px; ">You might be interested in traveling to...</h1>
+            <table style="margin: 0 auto;" cellspacing="40">
+                <tr>
+                    <td>
+                        <h1 style="color:#f1c40f;font-family:arial; font-size: 50px;">{avdest[0]}<h1>
+                    </td>
+                    <td>
+                        <h1 style="color:#f1c40f;font-family:arial font-size: 50px;" >{avdest[1]}<h1>
+                    </td>
+                    <td>
+                        <h1 style="color:#f1c40f;font-family:arial; font-size: 50px; ">{avdest[2]}<h1>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img width="400" height="300" src="{avurl[0]}" alt="travel">
+                    </td>
+                    <td>
+                        <img width="400" height="300" src="{avurl[1]}" alt="travel">
+                    </td>
+                    <td>
+                        <img width="400" height="300" src="{avurl[2]}" alt="travel">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivalent to {int((avco2[0]/co2)+1)} times this same journey in CO2 expenditure by plane and you would save {round(int((avco2[0]/co2)+1)*fuel,2)}€ <h2>
+                    </td>
+                    <td>
+                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivalent to {int((avco2[1]/co2)+1)} times this same journey in CO2 expenditure by plane and you would save {round(int((avco2[1]/co2)+1)*fuel,2)}€ <h2>
+                    </td>
+                    <td>
+                        <h2 style="color:#252525;font-family:arial; font-size: 20px;" >Equivalent to {int((avco2[2]/co2)+1)} times this same journey in CO2 expenditure by plane and you would save {round(int((avco2[2]/co2)+1)*fuel,2)}€ <h2>
                     </td>
                 </tr>
             </table>
